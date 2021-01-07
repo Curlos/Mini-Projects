9c98793d1fcf5249089da5f8c7796b6ca7506b6c
Carlos Martinez <curlosmart@gmail.com>
2021-01-07 11:17:14 -0500

    Bubble Sort (complete)

---
+++ Classic Algorithms/Sorting/bubbleSort.py
@@ -0,0 +1,23 @@
+# Python program for implementation of Bubble Sort
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    # Traverse through all the array elements
+    for i in range(n - 1):
+        # range(n) also works but outler loop will repeat one more time than needed
+        # Last i elemnents are already in place
+        for j in range(0, n - i - 1):
+
+            # traverse the array from 0 to n - i - 1
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+print(f"Unsorted array is: {arr}")
+
+bubbleSort(arr)
+print(f"Sorted array is: {arr}")
